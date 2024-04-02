015117e1ca27e235b9b940a9654682191a050121
amirh4336 <amirhossienheydarpoor@gmail.com>
2024-04-03 02:11:27 +0330

    feat: send req to back for create tweet

--- src/app/_components/newTweetForm.tsx
+++ src/app/_components/newTweetForm.tsx
@@ -3,6 +3,7 @@ import { useSession } from "next-auth/react";
 import Button from "./button";
 import ProfileImage from "./profileImage";
 import { useCallback, useLayoutEffect, useRef, useState } from "react";
+import { api } from "~/trpc/react";
 
 function updateTextAreaSize(textArea?: HTMLTextAreaElement) {
   if (textArea == null) return;
@@ -14,23 +15,34 @@ const NewTweetForm = () => {
   const session = useSession();
   const [inputValue, setInputValue] = useState("");
   const textAreaRef = useRef<HTMLTextAreaElement>();
-  const inputRef = useCallback(
-    (textArea : HTMLTextAreaElement) => {
-      updateTextAreaSize(textArea)
-      textAreaRef.current = textArea
-    },
-    [],
-  )
-  
+  const inputRef = useCallback((textArea: HTMLTextAreaElement) => {
+    updateTextAreaSize(textArea);
+    textAreaRef.current = textArea;
+  }, []);
 
   useLayoutEffect(() => {
     updateTextAreaSize(textAreaRef.current);
   }, [inputValue]);
 
+  const createTweet = api.tweet.create.useMutation({
+    onSuccess: (newTweet) => {
+      console.log(newTweet);
+      setInputValue("")
+    },
+  });
+
   if (session.status !== "authenticated") return null;
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    createTweet.mutate({ content: inputValue });
+  };
+
   return (
-    <form className="flex flex-col gap-2 border-b px-4 py-2">
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col gap-2 border-b px-4 py-2"
+    >
       <div className="flex gap-4">
         <ProfileImage src={session.data.user.image} />
         <textarea
