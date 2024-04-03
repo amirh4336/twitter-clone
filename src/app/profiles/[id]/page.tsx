7148d57be6f1e0e5344e43525de16179d1c31097
amirh4336 <amirhossienheydarpoor@gmail.com>
2024-04-03 21:47:30 +0330

    feat: add new route infiniteProfileFeed  to api

--- src/app/profiles/[id]/page.tsx
+++ src/app/profiles/[id]/page.tsx
@@ -1,32 +1,75 @@
-import type { GetStaticPathsContext } from "next";
-import Head from "next/head";
-import { ssgHelper } from "~/server/api/ssgHelper";
+import type { Metadata, NextPage } from "next";
+import Link from "next/link";
+import { VscArrowLeft } from "react-icons/vsc";
+import IconHoverEffect from "~/app/_components/iconHoverEffect";
+import InfiniteTweetList from "~/app/_components/infiniteTweetList";
+import ProfileImage from "~/app/_components/profileImage";
 
-const Page = () => {
+import { api } from "~/trpc/server";
+
+
+type Props = {
+  params: { id: string };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  // read route params
+  const id = params.id;
+
+  // fetch data
+  const profile = await api.profile.getById({
+    id,
+  });
+
+  return {
+    title: `Twitter Clone - ${profile?.name}`,
+  };
+}
+
+const Page: NextPage<Props> = async ({ params }) => {
+  const profile = await api.profile.getById({
+    id: params.id,
+  });
+
+  if (profile == null ?? profile?.name == null) return <p>Opss we have a problem</p>;
+
   return (
     <>
-      <Head>
-        <title>{`Twitter Clone ${user.name}`}</title>
-      </Head>
+      <header className="sticky top-0 z-10 flex item-center border-b bg-white px-4 p-2" >
+        <Link href=".." className="mr-2">
+        <IconHoverEffect>
+          <VscArrowLeft className="h-6 w-6" />
+        </IconHoverEffect>
+        </Link>
+        <ProfileImage src={profile.image} className="flex-shrink-0" />
+        <div className="ml-2 flex-grow">
+          <h1 className="text-lg font-bold">{profile.name}</h1>
+          <div className="text-gray-500">
+            {profile.tweetsCount} {" "}
+            {getPlural(profile.tweetsCount, "Tweet", "Tweets")} -{" "}
+            {profile.followersCount} {" "}
+            {getPlural(profile.followersCount, "Follower", "Followers")} -{" "}
+            {profile.followsCount} Following
+          </div>
+          <FollowButton isFollowing={profile.isFollowing} userId={id} onClick={() => null} />
+        </div>
+      </header>
+      <main>
+        <InfiniteTweetList />
+      </main>
     </>
   );
 };
 
-export async function GetStaticProps(context: GetStaticPathsContext<{id : string}>) {
-  const id = context.params.id;
+const FollowButton = () => {
+  return <h1>Follow</h1>
+}
 
-  if (id == null) {
-    return {
-      redirect : {
-        destination : "/"
-      }
-    }
-  }
 
-  const ssg = ssgHelper()
 
-  ssg.profile
-
+const pluralRules = new Intl.PluralRules()
+const getPlural = (number : number , singular: string, plural: string) => {
+  return pluralRules.select(parseInt(number.toString())) === "one" ? singular : plural 
 }
 
 export default Page;
--- src/server/api/routers/tweet.ts
+++ src/server/api/routers/tweet.ts
@@ -9,6 +9,25 @@ import {
 } from "~/server/api/trpc";
 
 export const tweetRouter = createTRPCRouter({
+  infiniteProfileFeed: publicProcedure
+    .input(
+      z.object({
+        userId: z.string(),
+        limit: z.number().optional(),
+        cursor: z.object({ id: z.string(), createdAt: z.string() }).optional(),
+      }),
+    )
+    .query(async ({ input: { limit = 10, userId, cursor }, ctx }) => {
+      return await getInfiniteTweets({
+        limit,
+        ctx,
+        cursor,
+        whereClause: {
+          userId,
+        },
+      });
+    }),
+
   infiniteFeed: publicProcedure
     .input(
       z.object({
@@ -19,12 +38,19 @@ export const tweetRouter = createTRPCRouter({
     )
     .query(
       async ({ input: { limit = 10, onlyFollowing = false, cursor }, ctx }) => {
+        const currnetUserId = ctx.session?.user.id;
 
-        const currnetUserId = ctx.session?.user.id
-
-        return await getInfiniteTweets({limit , ctx , cursor , whereClause: currnetUserId == null || !onlyFollowing ? undefined : {
-          user: { followers: { some: { id: currnetUserId } } }
-        }})
+        return await getInfiniteTweets({
+          limit,
+          ctx,
+          cursor,
+          whereClause:
+            currnetUserId == null || !onlyFollowing
+              ? undefined
+              : {
+                  user: { followers: { some: { id: currnetUserId } } },
+                },
+        });
       },
     ),
 
